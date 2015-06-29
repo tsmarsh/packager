78c08658a84df132921c127d1c097ee5c942d34f
Tom Marsh <ts.marsh@gmail.com>
2015-06-29 06:51:04 -0400

    WIP: starting to add containers

--- src/packager/core.clj
+++ src/packager/core.clj
@@ -1,13 +1,23 @@
 (ns packager.core
   (:require [schema.core :as s]))
 
-(def Vertex [(s/one Double "x")
-             (s/one Double "y")
-             (s/one Double "z")])
+
+(def Box [(s/one Double "x")
+             (s/one Double "y")])
+
+(def Shelf {:boxes [Box]
+            :root Box
+            :dimensions Box})
+
+(def Container {:shelves [Shelf]
+                :dimensions Box})
 
 (s/defn fit :- s/Bool
-  [container :- Vertex
-   object :- Vertex]
+  [container :- Box
+   object :- Box]
   (= 0 (->> (map - container object)
             (filter (partial > 0))
             count)))
+
+(s/defn remaining :- Box
+  [Shelf])
--- test/packager/core_test.clj
+++ test/packager/core_test.clj
@@ -3,11 +3,22 @@
             [packager.core :as c]
             [schema.test :as st]))
 
+()
 (deftest fitting
   (testing "does fit"
-    (let [machine [20.0 20.0 20.0]]
-      (is (c/fit machine [20.0 20.0 20.0]))))
+    (let [machine [20.0 20.0]]
+      (is (c/fit machine [20.0 20.0]))))
   
   (testing "doesn't fit"
-    (let [machine [19.0 20.0 20.0]]
-      (is (not (c/fit machine [20.0 20.0 20.0]))))))
+    (let [machine [19.0 20.0]]
+      (is (not (c/fit machine [20.0 20.0]))))))
+
+(deftest add
+  (testing "an empty container gets a shelf on which to place a box"
+    (let [c {:shelves []
+             :dimensions [20.0 20.0]}
+          box [19 19]
+          expected {:selves [{:boxes [box]
+                              :root [0.0 0.0]
+                              :dimensions [19.0 20.0]}]}]
+      (is (= expected  (c/add c box))))))
